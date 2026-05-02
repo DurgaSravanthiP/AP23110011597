c25856ab3f01f4c4042ae5a612042c6ca1a34510
DurgaSravanthiP <sravanthi9637@gmail.com>
2026-05-02 12:02:49 +0530

    final submission

--- vehicle_maintenance_scheduler/app.js
+++ vehicle_maintenance_scheduler/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { Log } from "./logging_middleware/logger.js";
+import { Log } from "../logging_middleware/logger.js";
 import axios from "axios";
 
 const app = express();
